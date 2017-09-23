7484d9f5b68c815ea3a66c00934b07c61f0e591a
MishyMinty <mustbemishtaken@gmail.com>
2017-09-23 11:23:33 +0300

    modified application.conf configuration

--- lib-project/app/models/UsersDBConnection.java
+++ lib-project/app/models/UsersDBConnection.java
@@ -1,30 +1,39 @@
 package models;
 
 import play.db.Database;
-import play.db.NamedDatabase;
 
 import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
-//inject users db not default
+
 @javax.inject.Singleton
 class UsersDBConnection {
 
     private Database db;
-    private DatabaseExecutionContext executionContext;
+    //private DatabaseExecutionContext executionContext;
 
     @Inject
-    public UsersDBConnection(Database db, DatabaseExecutionContext executionContext){
+    public UsersDBConnection(Database db){
         this.db = db;
-        this.executionContext = executionContext;
+        //this.executionContext = executionContext;
     }
-    public CompletionStage<Integer> displayConnection(){
-        return CompletableFuture.supplyAsync(() ->{
-            return db.withConnection(connection -> {
-                return 1;
-            });
-        }, executionContext);
+    public CompletableFuture<Void> displayConnection(){
+        return CompletableFuture.runAsync(() ->{
+            try {
+                Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:9000/?user=root&password=rootpassword");
+                PreparedStatement state = conn.prepareStatement("CREATE DATABASE playdbfirst");
+                int result = state.executeUpdate();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+           return;
+        });
 
     }
 }
