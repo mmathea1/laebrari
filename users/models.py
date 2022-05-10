60bf229e806ec2d170ad7e8d2740d9df06439a79
mingina <mathea.mingina@busaracenter.org>
2022-05-11 00:02:42 +0300

    added user authentication

--- users/models.py
+++ users/models.py
@@ -1,40 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-
-class User(models.Model):
+class User(AbstractUser):
     REQUIRED_FIELDS = ('email', 'password',)
     USERNAME_FIELD = 'username'
-
+    EMAIL_FIELD = 'email'
+    
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
+    phone_number = models.CharField(max_length=255, blank=False, null=False)
+    location = models.CharField(max_length=255, blank=False, null=False)
+    date_joined = models.DateTimeField(verbose_name="Date joined", auto_now_add=True, null=True)
     email = models.EmailField(max_length=255, blank=False, null=False)
     username = models.CharField(
         max_length=255, blank=False, null=False, unique=True)
     password = models.CharField(max_length=255, blank=False, null=False)
 
-    @property
-    def is_anonymous(self):
-        """
-        Always return False. This is a way of comparing User objects to
-        anonymous users.
-        """
-        return False
-
-    @property
-    def is_authenticated(self):
-        return False
-
     def __str__(self):
-        return "{} {}".format(self.email)
-
-
-class UserProfile(models.Model):
-    first_name = models.CharField(max_length=255, blank=False, null=False)
-    last_name = models.CharField(max_length=255, blank=False, null=False)
-    user = models.OneToOneField(
-        User, related_name='profile', unique=True, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=255, blank=False, null=False)
-    location = models.CharField(max_length=255, blank=False, null=False)
+        return "{} {}".format(self.first_name, self.last_name)
 
     objects = UserManager()
