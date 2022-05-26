5ee54904e0a16a6f1b7717b1b3037b66ec23e0e3
mingina <mathea.mingina@busaracenter.org>
2022-05-26 21:12:58 +0300

    cleaned up creating profiles

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
  path("signup/", views.user_registration, name="signup"),
- path('profile/', views.profile, name='profile')
+ path('profile/', views.profile, name='profile'),
+ path('home/', views.home, name='home')
 ]
--- users/views.py
+++ users/views.py
@@ -26,22 +26,18 @@ def user_registration(request):
     return render (request=request, template_name="users/signup.html", context={"register_form":form})
 
 def profile(request):
-    if request.method == "POST":
-        u_form = UserUpdateForm(request.POST)  
+    if request.method == "POST": 
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile) 
 
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
+        if p_form.is_valid():
             p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile') # Redirect back to profile page
     else:
-        u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
     
     context = {
         'user': request.user,
-        'u_form': u_form,
         'p_form': p_form
     }
 
