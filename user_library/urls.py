3cd97e808e74a6f48ee9547b4e5769bf2266bbfc
mingina <mathea.mingina@busaracenter.org>
2022-06-23 23:52:58 +0300

    adding a viewset for adding library book

--- user_library/urls.py
+++ user_library/urls.py
@@ -6,8 +6,10 @@ from user_library import views
 
 router = routers.DefaultRouter()
 router.register(r'library', views.UserLibraryViewSet, basename="library")
+# router.register(r'library/books', views.UpdateLibraryViewSet, basename="library_books")
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('library/books',views.UpdateLibraryViewSet, 'library_books')
 ]
