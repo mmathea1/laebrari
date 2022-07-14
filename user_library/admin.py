84e39c368ede9cb8e9dbdc594539171ee12c37be
mingina <mathea.mingina@busaracenter.org>
2022-07-14 23:36:18 +0300

    added Book and BookTransaction to admin site

--- user_library/admin.py
+++ user_library/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from user_library.models import UserLibrary
+from user_library.models import Book, BookTransaction, UserLibrary
 
 
 
 # Register your models here.
 admin.site.register(UserLibrary)
+admin.site.register(Book)
+admin.site.register(BookTransaction)
