8787b796fe86852408881d5687f4ad98985ccfaf
mingina <mathea.mingina@busaracenter.org>
2023-02-06 13:10:56 +0300

    enabled authentication

--- laebrari/urls.py
+++ laebrari/urls.py
@@ -8,26 +8,17 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from rest_framework import routers
 from django.urls import include, path
-from laebrari.views import CustomAuthTokenView
-from user_library.views import BookTransactionViewSet,  LibraryBookViewSet, UserLibraryViewSet
-from users.views import HomeView, ProfileView, ProfileViewSet, UserViewSet, user_registration
+from users.views import LoginUserView, RegisterUserView, UserDetailView
 from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename="user")
-router.register(r'librarians', ProfileViewSet, basename="librarian")
-router.register(r'libraries', UserLibraryViewSet, basename="library")
-router.register(r'books', LibraryBookViewSet, basename="library_books")
-router.register(r'transactions', BookTransactionViewSet, basename="book_transaction")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', CustomAuthTokenView.as_view()),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view()),
     path('api/', include(router.urls)),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', HomeView.as_view(), name='home'),
-    path('signup/', user_registration, name='signup'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('user-details/', UserDetailView.as_view(), name='user-detail')
 ]
 
 if settings.DEBUG:
--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,42 @@
 
 # from user_library.serializers import UserLibrarySerializer
-from users.models import Profile, User
+from users.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
-
+class LoginUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+    
+   
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = '__all__'
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True, validators=[UniqueValidator(User.objects.all())])
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(User.objects.all())])
+    password = serializers.CharField(required=True, write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(required=True, write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
 
-# class LibrarianSerializer(serializers.Serializer):
-#     library = UserLibrarySerializer()
-#     librarian = ProfileSerializer()
+
+    def validate(self,attrs):
+            if attrs['password'] != attrs['password2']:
+                raise serializers.ValidationError({"password": "Password fields did not match."})
+            return attrs
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+                username=validated_data['username'], 
+                email=validated_data['email'], password=validated_data['password'], 
+                first_name=validated_data['first_name'], 
+                last_name=validated_data['last_name']
+            )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
