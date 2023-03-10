b166427cdd4bdeddaf2193d21e51f661559f002b
mingina <mathea.mingina@busaracenter.org>
2023-03-10 15:18:33 +0300

    updated cors settings

--- laebrari/settings/base.py
+++ laebrari/settings/base.py
@@ -115,13 +115,13 @@ MEDIA_ROOT = BASE_DIR / 'media' # Directory where uploaded media is saved.
 MEDIA_URL = '/media/' # Public URL at the browser
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
      'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication', 
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
@@ -137,7 +137,10 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
+]
+CORS_ALLOW_CREDENTIALS = True
 
 
 
